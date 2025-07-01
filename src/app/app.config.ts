3ab29b70e8ca4e63e8c1d61561a870b0dfd76a15
AlexKaoforGit <alex840218@gmail.com>
2025-07-02 00:26:51 +0800

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
@@ -15,7 +15,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
--- src/app/todo/todo-module.ts
+++ src/app/todo/todo-module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TodoList } from './todo-list/todo-list';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [{ path: '', component: TodoList }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), TodoList],
+  imports: [RouterModule.forChild(routes), TodoList, MatIconModule],
   exports: [RouterModule],
 })
 export class TodoModule {}
